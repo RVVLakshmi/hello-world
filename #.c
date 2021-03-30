027a7cf97eb6798950b94c6cc782819bb923064a
RVVLakshmi <81034948+RVVLakshmi@users.noreply.github.com>
2021-03-30 20:23:19 +0530

    Create #.c

---
+++ #.c
@@ -0,0 +1,21 @@
+# include<studio.h>
+int main()
+{
+Printf("#####\n");
+Printf("#\n");
+Printf("####\n");
+Printf("#\n");
+Printf("#\n");
+Printf("#\n");
+Return 0;
+}
+
+
+
+Output:
+#####
+#
+####
+#
+#
+#
